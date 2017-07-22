ab219fd403b25eb9b346a3c5564f5d61d397dc61
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-07-22 16:46:01 -0700

    completed quote ranks assignment (sort not working yet)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from "./quote";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Quote Ranks';
+  quotes: Array<Quote> = [];
+  isValid: boolean = true;
+  numberKey: number = 0;
+
+  removeAt(array, index){
+    return array.removeAt(index);
+  }
+
+  voteUp(quoteid){
+    for (let i = 0; i < this.quotes.length; ++i){
+      if (this.quotes[i].id == quoteid){
+        return ++this.quotes[i].rating;
+      }
+    }
+  }
+  voteDown(quoteid){
+    for (let i = 0; i < this.quotes.length; ++i){
+      if (this.quotes[i].id == quoteid && this.quotes[i].rating > 0){
+        return --this.quotes[i].rating;
+      }
+    }
+  }
+  delete(quoteid){
+    for (let i = 0; i < this.quotes.length; ++i){
+      if (this.quotes[i].id == quoteid){
+        return this.removeAt(this.quotes, i);
+      }
+    }
+  }
+
+  addquote(quote){
+    this.quotes.push(quote);
+    this.numberKey += 1;
+  }
+
 }
---
+++ src/app/ranking/sort.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Quote } from '.././quote'
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(quotes: Array<Quote>): Array<Quote> {
+    quotes.sort((a: Quote, b: Quote) => {
+      if (a.rating < b.rating) {
+        return -1;
+      } else if (a.rating > b.rating) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return quotes;
+  }
+
+}
